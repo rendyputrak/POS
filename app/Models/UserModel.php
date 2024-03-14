ee6efefbd304b5c6a54db9f631f09761a770f072
rendyputrak <rendyputrak@gmail.com>
2024-03-14 23:24:34 +0700

    Jobsheet 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\LevelModel;
 
 class UserModel extends Model
 {
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
---
+++ resources/views/user_ubah.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Ubah Data User</title>
+</head>
+<body>
+    <form action="/POS/public/user/ubah_simpan/{{$data->user_id}}" method="post">
+    {{ csrf_field() }}
+    {{ method_field('PUT') }}
+        <label>Username</label>
+        <input type="text" name="username" value="{{$data->username}}">
+        <br>
+        <label>Nama</label>
+        <input type="text" name="nama" value="{{$data->nama}}">
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" value="{{$data->password}}">
+        <br>
+        <label>Level ID</label>
+        <input type="number" name="level_id" value="{{$data->level_id}}">
+        <br><br>
+        <input type="submit" class="btn btn-success" value="Ubah">
+    </form>
+</body>
+</html>
